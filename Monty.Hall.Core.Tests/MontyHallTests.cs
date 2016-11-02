5dbf20a2a06e75a041eaed302f05e322c5246a49
Lee Barnard <lee.barnard@man.com>
2016-11-02 10:58:37 +0000

    Initial commit

---
+++ Monty.Hall.Core.Tests/MontyHallTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Monty.Hall.Core.Tests
+{
+    [TestFixture]
+    public class MontyHallTests
+    {
+        [TestCase(3, 1000)]
+        public void SwapWins(int numberOfDoors, int gamesToPlay)
+        {
+            Game game = new Game(numberOfDoors);
+
+            var wins = 0;
+            var losses = 0;
+            for (int i = 0; i < gamesToPlay; i++)
+            {
+                if (game.Swap())
+                    wins++;
+                else
+                {
+                    losses++;
+                }
+            }
+            Assert.That(losses != 0, "You didn't lose the game at all");
+            Assert.AreEqual(0.6m, (decimal)wins / (decimal)losses, "The percentage of wins isn't correct");
+        }
+
+        [TestCase(3, 100000)]
+        public void StayWins(int numberOfDoors, int gamesToPlay)
+        {
+            Game game = new Game(numberOfDoors);
+
+            var wins = 0;
+            var losses = 0;
+            for (int i = 0; i < gamesToPlay; i++)
+            {
+                if (game.Stay())
+                    wins++;
+                else
+                {
+                    losses++;
+                }
+            }
+            Assert.That(losses != 0, "You didn't lose the game at all");
+            Assert.AreEqual(0.6m, (decimal)wins / (decimal)losses, "The percentage of wins isn't correct");
+        }
+
+    }
+
+
+
+
+}
---
+++ Monty.Hall.Core/Game.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monty.Hall.Core
+{
+    public class Game
+    {
+        public List<Prize> Doors = new List<Prize>();
+        private readonly int _noOfDoors;
+        private readonly Random _rnd = new Random(DateTime.Now.Millisecond);
+        public Game(int noOfDoors)
+        {
+            _noOfDoors = noOfDoors;
+            Init();
+        }
+
+        private void Init()
+        {
+            // fill the doors with goats
+            for (int door = 0; door < _noOfDoors; door++)
+            {
+                Doors.Add(Prize.Goat);
+            }
+            // pick random door for the car
+            Doors[_rnd.Next(0, _noOfDoors - 1)] = Prize.Car;
+        }
+
+        public bool Swap()
+        {
+            // Select a random door
+            var selectedDoor = _rnd.Next(0, _noOfDoors - 1);
+
+            // Of the non-selected door, identify the first one with a goat
+
+            int goatDoor = -1;
+            int swappedDoor = -1;
+
+            for (int currentDoor = 0; currentDoor < _noOfDoors ; currentDoor++)
+            {
+                if (currentDoor == selectedDoor)
+                    continue;
+
+                if (Doors[currentDoor] == Prize.Goat)
+                {
+                    goatDoor = currentDoor;
+                    break;
+                }
+            }
+
+            for (int currentDoor = 0; currentDoor < _noOfDoors; currentDoor++)
+            {
+                if (currentDoor == selectedDoor || currentDoor == goatDoor)
+                    continue;
+
+                return Doors[currentDoor] == Prize.Car;
+
+            }
+            return true;
+        }
+
+        public bool Stay()
+        {
+            return Doors[_rnd.Next(0, _noOfDoors - 1)] == Prize.Car;
+        }
+
+    }
+
+    public enum Prize
+    {
+        Goat = 0,
+        Car = 1
+    }
+}
